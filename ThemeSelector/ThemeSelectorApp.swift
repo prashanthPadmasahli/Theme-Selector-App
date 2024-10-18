47d202877153ed887c89ef157d12099cb5705e61
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-10-18 17:29:30 +0530

    Initial Commit

---
+++ ThemeSelector/ThemeSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeSelectorApp.swift
+//  ThemeSelector
+//
+//  Created by mac on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ThemeSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
